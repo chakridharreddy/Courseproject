fd8847c6cfbb8f950b0dadc3ffc625e79a17293b
chakridharreddy <91155126+chakridharreddy@users.noreply.github.com>
2021-09-21 19:24:30 -0400

    my commits

---
+++ Project/demo/src/main/java/com/cis634/demo/shopping/demo/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.cis634.demo.shopping.demo.repository;
+
+import com.cis634.demo.shopping.demo.domain.Product;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Product entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ Project/demo/src/main/java/com/cis634/demo/shopping/demo/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.cis634.demo.store.web.rest.vm;
