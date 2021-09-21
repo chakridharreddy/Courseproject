508a224494a754e877a9c743102f49f94819de8d
Tulasi priyanka chirumamilla <89858100+priya010197@users.noreply.github.com>
2021-09-21 17:22:09 -0400

    Add files via upload

---
+++ Project/demo/src/main/java/com/cis634/demo/shopping/demo/Cis634OopApplication.java
@@ -0,0 +1,13 @@
+package com.cis634.demo.shopping.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cis634OopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cis634OopApplication.class, args);
+	}
+
+}
